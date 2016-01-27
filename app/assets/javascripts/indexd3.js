4c91b6a9ad2d5d1df428010abbd7e9ac57f913e2
Bryce Lively <bryce.lively@gmail.com>
2016-01-27 15:27:24 -0500

    self = select(this)

--- app/assets/javascripts/indexd3.js
+++ app/assets/javascripts/indexd3.js
@@ -3,17 +3,21 @@ $(document).ready(function() {
     var number = 0.00;
     var totalmissing = 0;
     var percentTotalMissing = 0;
+    // dataArray needs to be an Array of Arrays [number, title]
     var dataArray = [];
-    var labelArray = [];
+    // var labelArray = [];
+    var chartHeight = 1000;
+    var barDataDivisor = 1000000;
+    var initialBarColor = "lightgreen";
+    var secondaryBarColor = "yellow";
 
     // This is populating the arrays with data on the contracts
     function assignData(contract) {
-        labelArray.push(contract.title);
         if (!contract.dollar_amt) {
-            dataArray.push(0);
+            dataArray.push([0, contract.title]);
             totalmissing++;
         } else {
-            dataArray.push(parseInt(contract.dollar_amt));
+            dataArray.push([parseInt(contract.dollar_amt), contract.title]);
             number -= parseFloat(-contract.dollar_amt).toFixed(2);
             totalNum = (number).toLocaleString("currency", "USD");
         }
@@ -25,14 +29,23 @@ $(document).ready(function() {
     var svgArea = d3.select("#d3me")
         .append("svg")
         .attr("width", 100 + "%")
-        .attr("height", 1000);
+        .attr("height", chartHeight);
     var bars = svgArea.selectAll("rect")
         .data(dataArray)
         .enter()
-        .append("rect")
+        .append("svg:rect")
     var barAttr = bars.attr("class", "bar")
         .attr("width", 2)
-        .attr("height", function(d){return d / 1000000})
-        .style("fill", "lightgreen")
-        .style("x", function(d,i){return i * 2});
+        .attr("height", function(d){return d[0] / barDataDivisor})
+        .style("fill", initialBarColor)
+        .attr("y", function(d) {
+            return chartHeight - d[0] / barDataDivisor;
+        })
+        .style("x", function(d,i){return i * 2})
+        .on("mouseover", function(){
+            return d3.select(this).style("fill", secondaryBarColor)})
+        .on("mouseout", function(){
+            return d3.select(this).style("fill", initialBarColor)});
+    var titles = barAttr.append("svg:title")
+        .text(function(d) { return d[1]; });
 });
