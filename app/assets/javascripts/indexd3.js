db566102e99ebaa0bac49f6a774e6047e9edf3ef
Bryce Lively <bryce.lively@gmail.com>
2016-01-27 16:01:04 -0500

    tooltip

--- app/assets/javascripts/indexd3.js
+++ app/assets/javascripts/indexd3.js
@@ -3,13 +3,12 @@ $(document).ready(function() {
     var number = 0.00;
     var totalmissing = 0;
     var percentTotalMissing = 0;
-    // dataArray needs to be an Array of Arrays [number, title]
+    // dataArray is an Array of Arrays [number, title]
     var dataArray = [];
-    // var labelArray = [];
-    var chartHeight = 1000;
-    var barDataDivisor = 1000000;
+    var chartHeight = 300;
+    var barDataDivisor = 100000;
     var initialBarColor = "lightgreen";
-    var secondaryBarColor = "yellow";
+    var secondaryBarColor = "red";
 
     // This is populating the arrays with data on the contracts
     function assignData(contract) {
@@ -26,26 +25,50 @@ $(document).ready(function() {
     JSON.parse($("#contracts_json").html()).forEach(function(contract) {
         assignData(contract);
     });
+
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .text("a simple tooltip");
+
     var svgArea = d3.select("#d3me")
         .append("svg")
         .attr("width", 100 + "%")
         .attr("height", chartHeight);
+
     var bars = svgArea.selectAll("rect")
         .data(dataArray)
         .enter()
         .append("svg:rect")
+
     var barAttr = bars.attr("class", "bar")
         .attr("width", 2)
-        .attr("height", function(d){return d[0] / barDataDivisor})
+        .attr("height", function(d){return Math.sqrt(d[0] / barDataDivisor)})
         .style("fill", initialBarColor)
         .attr("y", function(d) {
-            return chartHeight - d[0] / barDataDivisor;
+            return chartHeight - Math.sqrt(d[0] / barDataDivisor);
         })
         .style("x", function(d,i){return i * 2})
         .on("mouseover", function(){
             return d3.select(this).style("fill", secondaryBarColor)})
         .on("mouseout", function(){
             return d3.select(this).style("fill", initialBarColor)});
+
     var titles = barAttr.append("svg:title")
         .text(function(d) { return d[1]; });
+
+    var barTooltips = bars
+        .on("mouseover", function(){
+            var targetHtml = d3.select(this)[0][0].textContent;
+            return tooltip.text(targetHtml)
+            .style("visibility", "visible")
+            .style("background", "rgba(0, 0, 0, 0.4)");})
+        .on("mousemove", function(){
+            return tooltip.style("top",
+(d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+        .on("mouseout", function(){
+            return tooltip.style("visibility", "hidden");
+    });
 });
