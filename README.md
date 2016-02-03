e1efc5cf6dd5a8fa84ccc9fa2a0d920c7d1f5136
Bryce Lively <bryce.lively@gmail.com>
2016-02-03 12:30:02 -0500

    seeds with contract-agency relations

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Concept:
 This app will display, in an easily navigable and consumer-friendly way, information and analysis of U.S. defense spending, using up-to-date information.  
 
 How:
-To do this, it draws data from a public DoD RSS feed that is updated nearly every workday at around 6pm EST. That public information is then parsed, cleaned, and transformed into a multi-model application that (will) allow allow the user to easily understand the content being released by the U.S. government.  
+To do this, it draws data from a public DoD RSS feed that is updated nearly every workday at around 6pm EST. That public information is then parsed, cleaned, and transformed into a multi-model application that (will) allow the user to easily understand the content being released by the U.S. government.  
 
 Intent:
 The purpose of this is to fulfill the mandate of open government data by 1) improving access to this information and 2) allowing citizens to understand how their tax dollars are being spent. Defense spending is a highly politicized issue, and is often discussed without adequate knowledge or context. This application will solve that knowledge gap.
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,26 @@ require 'open-uri'
 Contract.destroy_all
 Agency.destroy_all
 
-doc = Nokogiri::XML(open('http://www.defense.gov/DesktopModules/ArticleCS/RSS.ashx?max=1095&ContentType=400&Site=727')) do |config|
+doc = Nokogiri::XML(open('http://www.defense.gov/DesktopModules/ArticleCS/RSS.ashx?max=3650&ContentType=400&Site=727')) do |config|
     config.options = Nokogiri::XML::ParseOptions::NONET
 end
 
 days = doc.xpath('//item')
+
+# works for 98.72773536895674% of days.
+def findAgency(nok)
+    agencyName = /[A-Z\. ]+/.match(nok.inner_text.split('CONTRACTS')[1].to_s).to_s
+    theOne = Agency.find_by_name agencyName
+    if theOne === nil
+        return theOne
+    else
+        return theOne.id
+    end
+end
+
+for day in days do
+    Agency.create(name: /[A-Z\. ]+/.match(day.xpath('description').inner_text.split('CONTRACTS')[1].to_s))
+end
 
 for day in days do
 
@@ -19,13 +34,15 @@ for day in days do
         money = nil
     end
 
+    Agency.create(name: day.xpath('description').to_s.split('CONTRACTS	')[1].to_s.split('	')[0])
+
     Contract.create(title: day.xpath('title').to_s.split("\n")[1],
                     link: day.xpath('link').to_s.split('>')[1].split('<')[0],
                     description: day.xpath('description').to_s.split("\n")[1],
                     pubdate: DateTime.httpdate(day.xpath('pubDate').to_s.split('>')[1].split('<')[0]),
                     dollar_amt: money,
-                    creator: day.xpath('dc:creator').to_s.split('>')[1].split('<')[0])
-    Agency.create(name: day.xpath('description').to_s.split('CONTRACTS	')[1].to_s.split('	')[0])
+                    creator: day.xpath('dc:creator').to_s.split('>')[1].split('<')[0],
+                    agency_id: findAgency(day.xpath('description')))
 end
 
 agencies = Agency.all
