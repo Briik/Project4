287f36638108242d39b3b234d5ab1254a3c2dfcd
Bryce Lively <bryce.lively@gmail.com>
2016-03-02 11:55:03 -0500

    tests & .yml

---
+++ test/models/agency_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AgencyTest < ActiveSupport::TestCase
+    test "test for Agencies exists" do
+    assert true
+    end
+end
---
+++ test/models/contract_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ContractTest < ActiveSupport::TestCase
+    test "test for Contracts exists" do
+    assert true
+    end
+end
